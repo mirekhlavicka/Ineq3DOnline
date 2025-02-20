b815bfb0b1bf5dff719fd4130e7a6a6f13b46bf7
Hlavicka <Hlavicka@DESKTOP-EIH729J>
2025-02-20 17:26:35 +0100

    Commit 20250220

--- Ineq3DOnline/Models/Tools/SignedDistance.cs
+++ Ineq3DOnline/Models/Tools/SignedDistance.cs
@@ -11,13 +11,41 @@ namespace Ineq3DOnline
 	public class SignedDistance
 	{
         private TriangleMeshInterop.TriangleMeshWrapper meshWrapper = null;
-        public SignedDistance(string filePath)
+        public SignedDistance(string filePath, double d = 2, double x0 = 0, double y0 = 0, double z0 = 0)
 		{
             List<double[]> v = null;
             List<int[]> t = null;
             filePath = System.IO.Path.Combine(HttpContext.Current.Server.MapPath("~/MeshSamples"), filePath);
 
             STLLoader.LoadSTL(filePath, out v, out t);
+
+            if (d > 0)
+            {
+                double minX = double.MaxValue, minY = double.MaxValue, minZ = double.MaxValue;
+                double maxX = -double.MaxValue, maxY = -double.MaxValue, maxZ = -double.MaxValue;
+
+                foreach (var p in v)
+                {
+                    minX = Math.Min(minX, p[0]);
+                    minY = Math.Min(minY, p[1]);
+                    minZ = Math.Min(minZ, p[2]);
+                    maxX = Math.Max(maxX, p[0]);
+                    maxY = Math.Max(maxY, p[1]);
+                    maxZ = Math.Max(maxZ, p[2]);
+                }
+
+                double s = Math.Min(Math.Min(d / (maxX - minX), d / (maxY - minY)), d / (maxZ - minZ));
+                double x00 = (maxX + minX) / 2.0d;
+                double y00 = (maxY + minY) / 2.0d;
+                double z00 = (maxZ + minZ) / 2.0d;
+
+                foreach (var p in v)
+                {
+                    p[0] = x0 + s * (p[0] - x00);
+                    p[1] = y0 + s * (p[1] - y00);
+                    p[2] = z0 + s * (p[2] - z00);
+                }
+            }
 
             meshWrapper = new TriangleMeshInterop.TriangleMeshWrapper(v, t);
         }
